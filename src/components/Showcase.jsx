244b9a7e62b72a2e1cc6634b4609b2ee1505c1b8
Meek Karan <meekkaran@gmail.com>
2021-01-15 10:30:28 +0300

    first commit

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Showcase = () => {
+    return( <div className="showcase">
+        <h2>Discover an adventure</h2>
+        <h1>Travel the world</h1>
+        <p>Lorem ipsum dolor sit amet consectetur
+             adipisicing elit. Commodi, nihil?
+        </p>
+
+        <div className="showcase__buttons">
+            <button className="showcase__btn-services">SERVICES</button>
+            <button className="showcase__btn-about">ABOUT US</button>
+        </div>
+    </div>
+    
+    );
+};
+
+export default Showcase;
